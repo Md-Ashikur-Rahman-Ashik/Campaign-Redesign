d9689c90f1fb1f03ebdee218fa1fd6eb439b52c3
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2025-10-13 00:16:03 +0000

    Adding types for navbar

---
+++ src/app/types/constants.ts
@@ -0,0 +1,18 @@
+export interface NavItem {
+  id: number;
+  name: string;
+  href: string;
+}
+
+export const NAV_LINKS: NavItem[] = [
+  { id: 1, name: "প্রার্থী", href: "/candidate" },
+  { id: 2, name: "ইশতেহার", href: "/manifesto" },
+  { id: 3, name: "কার্যক্রম", href: "/activities" },
+  { id: 4, name: "গ্যালারী", href: "/gallery" },
+];
+
+
+export const LOGO_LINKS = {
+  jaksue: '/images/jaksue_logo.png',
+  election: '/images/election_title.png',
+};

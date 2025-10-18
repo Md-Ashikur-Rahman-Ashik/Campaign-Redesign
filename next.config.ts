a599d67cda56db2982806454e36f308d7945097d
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2025-10-18 13:49:40 +0000

    Modified image links

--- next.config.ts
+++ next.config.ts
@@ -11,10 +11,6 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'i.ibb.co',
       },
-      {
-        protocol: 'https',
-        hostname: 'static.combinedstudentsalliance.com',
-      }
     ],
   },
 };
--- src/types/issues.ts
+++ src/types/issues.ts
@@ -8,26 +8,26 @@ export const ISSUE_DATA: Issue[] = [
   {
     id: 1,
     link: '/issues/1',
-    imageUrl: 'https://static.combinedstudentsalliance.com/uploads/store/agenda/fe896949-7ccb-436f-8a68-3825fe52726c/image/800-314a67c8522305c2ad59949899253b31.jpg',
+    imageUrl: 'https://i.ibb.co/DgShGPGC/FirstImg.jpg',
   },
   {
     id: 2,
     link: '/issues/2',
-    imageUrl: 'https://static.combinedstudentsalliance.com/uploads/store/agenda/9c7f84ae-aa46-45b9-9b3f-0196a2206208/image/1200-ac2eb3ee24478c2ef56adf7c515fdfb6.jpg',
+    imageUrl: 'https://i.ibb.co/Y7VBD2KQ/Second-Img.jpg',
   },
   {
     id: 3,
     link: '/issues/3',
-    imageUrl: 'https://static.combinedstudentsalliance.com/uploads/store/agenda/8092fcca-5685-41c3-b7ac-4aba888b1fd3/image/800-9b549a9c158d5127873907679216a7f8.jpg',
+    imageUrl: 'https://i.ibb.co/WvJY8PXY/ThirdImg.jpg',
   },
   {
     id: 4,
     link: '/issues/4',
-    imageUrl: 'https://static.combinedstudentsalliance.com/uploads/store/agenda/5666450e-09bb-4478-a843-a4357a62ffb0/image/800-c385874bc82348a5a312a9d4cffe6855.jpg',
+    imageUrl: 'https://i.ibb.co/qM10jRGR/Fourth-Img.jpg',
   },
   {
     id: 5,
     link: '/issues/5',
-    imageUrl: 'https://static.combinedstudentsalliance.com/uploads/store/agenda/47ef8c3c-5042-4fb5-bedb-0acb3967ef2e/image/800-fe6185374b28dca008cd6731108a8dbe.jpg',
+    imageUrl: 'https://i.ibb.co/TDRHmXQB/FifthImg.jpg',
   },
 ];

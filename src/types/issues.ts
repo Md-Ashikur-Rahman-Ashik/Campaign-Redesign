83a03fc46fa2634deab886f6248d660b78f2ba9a
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2025-10-27 11:57:00 +0000

    Added data for issues

--- src/types/issues.ts
+++ src/types/issues.ts
@@ -2,6 +2,7 @@ export type Issue = {
   id: number;
   link: string;
   imageUrl: string;
+  detailsImageUrl: string;
 };
 
 export const ISSUE_DATA: Issue[] = [
@@ -9,25 +10,30 @@ export const ISSUE_DATA: Issue[] = [
     id: 1,
     link: '/issues/1',
     imageUrl: 'https://i.ibb.co/DgShGPGC/FirstImg.jpg',
+    detailsImageUrl: 'https://i.ibb.co/DgShGPGC/FirstImg.jpg',
   },
   {
     id: 2,
     link: '/issues/2',
     imageUrl: 'https://i.ibb.co/Y7VBD2KQ/Second-Img.jpg',
+    detailsImageUrl: 'https://i.ibb.co/Y7VBD2KQ/Second-Img.jpg',
   },
   {
     id: 3,
     link: '/issues/3',
     imageUrl: 'https://i.ibb.co/WvJY8PXY/ThirdImg.jpg',
+    detailsImageUrl: 'https://i.ibb.co/WvJY8PXY/ThirdImg.jpg',
   },
   {
     id: 4,
     link: '/issues/4',
     imageUrl: 'https://i.ibb.co/qM10jRGR/Fourth-Img.jpg',
+    detailsImageUrl : 'https://i.ibb.co/qM10jRGR/Fourth-Img.jpg',
   },
   {
     id: 5,
     link: '/issues/5',
     imageUrl: 'https://i.ibb.co/TDRHmXQB/FifthImg.jpg',
+    detailsImageUrl : 'https://i.ibb.co/TDRHmXQB/FifthImg.jpg',
   },
 ];

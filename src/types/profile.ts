366d655d339d2f9e3cdf242f243b14b61d7fa6b9
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2025-10-28 11:07:39 +0000

    Added candidate data

---
+++ src/types/profile.ts
@@ -0,0 +1,17 @@
+export interface ProfileData {
+  title: string;
+  description: string;
+  buttonLink: string;
+  imageUrl: string;
+  imageAlt: string;
+}
+
+
+export const candidateData: ProfileData = {
+  title: "প্রার্থীর পরিচিতি",
+  description:
+    "কাজী দিন মোহাম্মদ একজন সমাজসেবক ও ইসলামী মূল্যবোধে অনুপ্রাণিত নেতা। যিনি ন্যায়, সেবা ও মানবিক উন্নয়নকে তাঁর রাজনৈতিক দর্শনের কেন্দ্রবিন্দুতে রেখেছেন।",
+  buttonLink: "#about",
+  imageUrl: "https://i.ibb.co/bVZRWV7/Candidate-Image.png",
+  imageAlt: "A portrait of the candidate",
+};

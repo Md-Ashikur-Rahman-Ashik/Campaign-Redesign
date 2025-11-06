f69d6fd85b1014b2949d3dc893936182aa69683c
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2025-11-06 05:44:14 +0000

    Made Card section hidden

--- src/app/manifesto/page.tsx
+++ src/app/manifesto/page.tsx
@@ -5,7 +5,7 @@ export default function Manifesto() {
     return (
         <>
             <HeroSection />
-            <IssueCardGrid />
+            {/* <IssueCardGrid /> */}
         </>
     );
 }
